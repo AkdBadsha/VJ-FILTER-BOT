03696ec59b9d97036e3691295e10a83d51e3849d
Bᴀᴅsʜᴀ Sᴛᴜᴅɪᴏs <141768309+AkdBadsha@users.noreply.github.com>
2024-08-25 01:52:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,8 +52,8 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://AutoMed:pass@cluster0.id2n0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Med")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'MedOne')
+DATABASE_NAME = environ.get('DATABASE_NAME', "Meed")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'MedOnoe')
 
 # Premium And Referal Settings
 PREMIUM_AND_REFERAL_MODE = bool(environ.get('PREMIUM_AND_REFERAL_MODE', False)) # Set Ture Or False
